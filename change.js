3cadd1e8f4dd1ae6cc541b9531957e028edfea9f
programmermohaimin <you@example.com>
2022-04-02 11:40:33 +0600

    js practice

---
+++ change.js
@@ -0,0 +1,15 @@
+// var myName = "Programmer mohaimin";
+// console.log(myName.length);
+// if(myName.length > 120){
+//     console.log("kutta mohaimin");
+// }
+// console.log(myName);
+// else{
+//     console.log("Programmer Mohaimin");
+// }
+
+
+var myName = "Programmer Mohaimin";
+if (myName.length > 12) {
+    console.log("Your Name is changed now your name is mohaimin ");
+}
---
+++ inchToFeet.js
@@ -0,0 +1,24 @@
+
+// function inchToFeet(inch){
+//     var feet = inch/12;
+//     return feet;
+// }
+
+// abbu = inchToFeet(120);
+// console.log(abbu , "abbu");
+
+// ammu = inchToFeet(150);
+// console.log(ammu , "ammu");
+
+
+// apu = inchToFeet(220);
+// console.log(apu , "apu");
+
+
+function inchToFeet(inch) {
+    var feet = inch/10;
+    return feet;
+}
+
+abbu = inchToFeet(100);
+console.log(abbu);
